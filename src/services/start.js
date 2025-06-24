9c9ff3e6e395ad1621c58a88d4bd74c115efa012
Daniel Rivas <daniel.rivas@osigu.com>
2025-06-24 10:41:36 -0600

    feat: se añaden estadisticas en la pagina de dashboard

--- src/services/start.js
+++ src/services/start.js
@@ -3,6 +3,7 @@ import {showSuccess, showError, showAlert, showConfirmation} from '../utils/swee
 import {fetchData, fetchDataToken, sendData} from '../data/apiMethods.js';
 import {verificarToken} from '../utils/tokenValidation.js';
 import {mostrarToast} from '../utils/toast.js';
+import {USERS_API, TICKETS_API} from '../config/constants.js';
 
 const removeAllSessionStorage = async () => {
     sessionStorage.removeItem("token");
@@ -39,7 +40,53 @@ const checkTokenAndLoginInfo = async () => {
 };
 
 
+const obtainHeaders = () => {
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+        closeSession();
+        return null;
+    }
+    //retorna el token
+    return {"Authorization": `Bearer ${token}`};
+};
+
+
+const obtainUsersStatistics = async () => {
+    try {
+        const response = await fetchData(USERS_API, "GET", obtainHeaders());
+
+        if (response) {
+            document.getElementById("usuariosActivos").textContent = response.data.length || 0;
+        } else {
+            mostrarToast("No se trajeron datos.", "warning");
+        }
+    } catch (error) {
+        mostrarToast(error, "danger");
+    }
+};
+
+
+const obtainTicketsStatistics = async () => {
+    try {
+        const response = await fetchData(TICKETS_API, "GET", obtainHeaders());
+
+        if (response) {
+            document.getElementById("ticketsTotales").textContent = response.data.length || 0;
+            document.getElementById("ticketsNuevos").textContent = response.data.filter(ticket => ticket.status === "New").length;
+            document.getElementById("ticketsEnProcesos").textContent = response.data.filter(ticket => ticket.status === "En Proceso").length;
+            document.getElementById("ticketsResueltos").textContent = response.data.filter(ticket => ticket.status === "Resuelto").length;
+            document.getElementById("ticketsCerrados").textContent = response.data.filter(ticket => ticket.status === "Cerrado").length;
+        } else {
+            mostrarToast("No se trajeron datos.", "warning");
+        }
+    } catch (error) {
+        mostrarToast(error, "danger");
+    }
+};
+
+
 document.addEventListener("DOMContentLoaded", async() => {
+    
     // Variables
     const btnCerrarSesion = document.getElementById("btnCloseSession");
 
@@ -50,6 +97,8 @@ document.addEventListener("DOMContentLoaded", async() => {
 
     // Funciones a ejecutarse
     await checkTokenAndLoginInfo();
+    await obtainUsersStatistics();
+    await obtainTicketsStatistics();
 });
 
 
